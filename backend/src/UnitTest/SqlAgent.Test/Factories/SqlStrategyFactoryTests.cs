a0202387745a103a20f882a7348b827c52310acf
Gary <144909016+tse-wei-chen@users.noreply.github.com>
2026-08-23 10:57:50 +0800

    test(provider): make provider factory contract primary

--- backend/src/UnitTest/SqlAgent.Test/Factories/SqlStrategyFactoryTests.cs
+++ backend/src/UnitTest/SqlAgent.Test/Factories/SqlStrategyFactoryTests.cs
@@ -1,7 +1,8 @@
 using Moq;
+using SqlAgent.Service.Core.Providers;
 using SqlAgent.Service.Enums;
 using SqlAgent.Service.Factories;
-using SqlAgent.Service.Interfaces;
+using SqlAgent.Service.Models;
 using SqlAgent.Service.Strategies;
 using Xunit;
 
@@ -18,46 +19,60 @@ public class SqlStrategyFactoryTests
         var mockDb2 = new Mock<ISqlStrategy>();
         mockDb2.Setup(x => x.DbType).Returns(SqlAgentToolType.Postgres);
 
-        var strategies = new List<ISqlStrategy> { mockDb1.Object, mockDb2.Object };
-
-        Assert.Throws<InvalidOperationException>(() => new SqlStrategyFactory(strategies));
+        Assert.Throws<InvalidOperationException>(() =>
+            new SqlStrategyFactory([mockDb1.Object, mockDb2.Object]));
     }
 
     [Fact]
-    public void GetStrategy_WithValidType_ReturnsStrategy()
+    public void GetProvider_WithValidType_ReturnsCoreProvider()
     {
         var mockDb = new Mock<ISqlStrategy>();
         mockDb.Setup(x => x.DbType).Returns(SqlAgentToolType.Postgres);
+        var factory = new SqlStrategyFactory([mockDb.Object]);
 
-        var factory = new SqlStrategyFactory(new[] { mockDb.Object });
+        var result = factory.GetProvider(SqlAgentToolType.Postgres);
 
-        var result = factory.GetStrategy(SqlAgentToolType.Postgres);
-
-        Assert.Equal(mockDb.Object, result);
+        Assert.IsType<SqlProvider>(result);
+        Assert.Equal(SqlAgentToolType.Postgres, result.Type);
     }
 
     [Fact]
-    public void GetStrategy_WithInvalidType_ThrowsArgumentOutOfRangeException()
+    public void GetProvider_WithInvalidType_ThrowsArgumentOutOfRangeException()
     {
-        var factory = new SqlStrategyFactory(Array.Empty<ISqlStrategy>());
+        var factory = new SqlStrategyFactory([]);
 
-        Assert.Throws<ArgumentOutOfRangeException>(() => factory.GetStrategy(SqlAgentToolType.Postgres));
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+            factory.GetProvider(SqlAgentToolType.Postgres));
     }
 
     [Fact]
-    public void GetSupportedDatabaseTypes_ReturnsExpectedTypes()
+    public void GetSupportedProviderTypes_ReturnsExpectedTypes()
     {
         var mockDb1 = new Mock<ISqlStrategy>();
         mockDb1.Setup(x => x.DbType).Returns(SqlAgentToolType.Postgres);
         var mockDb2 = new Mock<ISqlStrategy>();
         mockDb2.Setup(x => x.DbType).Returns(SqlAgentToolType.MySQL);
+        var factory = new SqlStrategyFactory([mockDb1.Object, mockDb2.Object]);
 
-        var factory = new SqlStrategyFactory(new[] { mockDb1.Object, mockDb2.Object });
-
-        var types = factory.GetSupportedDatabaseTypes().ToList();
+        var types = factory.GetSupportedProviderTypes();
 
         Assert.Contains(SqlAgentToolType.Postgres, types);
         Assert.Contains(SqlAgentToolType.MySQL, types);
         Assert.Equal(2, types.Count);
+    }
+
+    [Fact]
+    public void BuildConnectionString_DelegatesWithoutExposingStrategy()
+    {
+        var model = new BuildDbConnectionModelBase { Host = "db.example" };
+        var mockDb = new Mock<ISqlStrategy>();
+        mockDb.Setup(x => x.DbType).Returns(SqlAgentToolType.Postgres);
+        mockDb.Setup(x => x.BuildConnectionString(model)).Returns("Host=db.example");
+        var factory = new SqlStrategyFactory([mockDb.Object]);
+
+        var result = factory.BuildConnectionString(SqlAgentToolType.Postgres, model);
+
+        Assert.Equal("Host=db.example", result);
+        mockDb.Verify(x => x.BuildConnectionString(model), Times.Once);
     }
 }
