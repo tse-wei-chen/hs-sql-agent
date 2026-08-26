27fea0eaa3a50a6fe75cbe9f06fa88b9a0319701
Gary <144909016+tse-wei-chen@users.noreply.github.com>
2026-08-26 15:24:33 +0800

    test: bridge remaining SQL core imports

--- backend/src/UnitTest/SqlAgent.Test/LegacySqlParsingNamespace.cs
+++ backend/src/UnitTest/SqlAgent.Test/LegacySqlParsingNamespace.cs
@@ -1,5 +1,23 @@
-namespace SqlAgent.Service.SqlParsing;
-
-internal static class LegacySqlParsingNamespaceMarker
-{
-}
+namespace SqlAgent.Service.Core.Analysis { }
+namespace SqlAgent.Service.Core.Ast { }
+namespace SqlAgent.Service.Core.Binding { }
+namespace SqlAgent.Service.Core.Compilation { }
+namespace SqlAgent.Service.Core.Lowering { }
+namespace SqlAgent.Service.Core.Mapping { }
+namespace SqlAgent.Service.Core.Normalization { }
+namespace SqlAgent.Service.Core.Pipeline { }
+namespace SqlAgent.Service.Enums { }
+namespace SqlAgent.Service.Models { }
+namespace SqlAgent.Service.SqlParsing { }
+namespace SqlAgent.Service.SqlTranslation { }
+namespace SqlAgent.Service.SqlTranslation.Ast { }
+namespace SqlAgent.Service.SqlTranslation.Ast.Semantic { }
+namespace SqlAgent.Service.SqlTranslation.DateFormats { }
+namespace SqlAgent.Service.SqlTranslation.Functions { }
+namespace SqlAgent.Service.SqlTranslation.Functions.Translators { }
+namespace SqlAgent.Service.SqlTranslation.Normalization { }
+namespace SqlAgent.Service.SqlTranslation.Semantic { }
+namespace SqlAgent.Service.SqlTranslation.TemplateAst { }
+namespace SqlAgent.Service.SqlTranslation.TemplateModifiers { }
+namespace SqlAgent.Service.SqlTranslation.TemplateParsing { }
+namespace SqlAgent.Service.SqlTranslation.TemplateResolution { }
